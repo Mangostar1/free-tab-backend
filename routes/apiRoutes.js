69687b6f5464aaa8ffb51d964e545a85e0c5f37d
Omar <93488662+Mangostar1@users.noreply.github.com>
2022-11-07 13:37:47 -0300

    Se hace coneccion a mysql y crea json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const dotenv = require('dotenv')
+dotenv.config();
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_NAME
+});
+
+connection.connect();
 
 router.get('/', (req, res) => {
     res.send('hola desde api home');
 })
 
-router.get('/api', (req, res) => {
-    res.send('hola desde api');
+router.get('/api/animals', (req, res) => {
+
+    connection.query('SELECT * FROM animales', function (error, results, fields) {
+        if (error) throw error;
+        res.json(results);//<-- Responde un json con la tabla user de mysql
+    });
+
+})
+
+router.get('/api/user', (req, res) => {
+
+    connection.query('SELECT * FROM user', function (error, results, fields) {
+        if (error) throw error;
+        res.json(results);//<-- Responde un json con la tabla user de mysql
+    });
+
 })
 
 
