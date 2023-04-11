42a16d576ae41f4ea99b0d8a30d5c5af52d98ab6
Omar <93488662+Mangostar1@users.noreply.github.com>
2023-04-11 16:52:08 -0400

    Se agrega nueva ruta a /api que permite el cierre de la session del usuario logeado.

--- api/signOut.js
+++ api/signOut.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 //*firebase
 const { initializeApp } = require("firebase/app");
-const { getAuth, signInWithEmailAndPassword  } = require("firebase/auth");
+const { getAuth } = require("firebase/auth");
 
 const firebaseConfig = {
 
@@ -22,9 +22,14 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-router.post('/api/logout', (req, res) => {
-    
-    //remove tocken
+router.get('/api/logout', (req, res) => {
+    auth.signOut()
+    .then(() => {
+        res.status(200).send("Sesión cerrada");
+    })
+    .catch((error) => {
+        res.status(500).send("Error al cerrar sesión", error);
+    })
 
 })
 
