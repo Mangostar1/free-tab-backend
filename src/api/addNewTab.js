984928c224f0ecdd9fa372a66271cbd93c3c0978
Omar <93488662+Mangostar1@users.noreply.github.com>
2023-06-03 16:21:58 -0400

    Se actualiza dependencia npm de firebase y se comienzan trabajos en la ruta para agregar nuevas tablaturas en firestore.

--- src/api/addNewTab.js
+++ src/api/addNewTab.js
@@ -4,16 +4,29 @@ const router = express.Router();
 const dotenv = require("dotenv");
 dotenv.config();
 
-const {
-  getFirestore,
-  Timestamp,
-  FieldValue,
-} = require("firebase-admin/firestore");
+//firebase
+const { getFirestore } = require("firebase/firestore");
+const app = require("../config/firebaseConfig.js");
+const db = getFirestore(app);
 
-router.post("api/new-tab", (res, req) => {
-  const { username, instrument, tab } = req.body;
+router.post("api/new-tab", async (req, res) => {
+  try {
+    const { userName, bandName, songnName, guitar, bass, guitarTab, bassTab } =
+      req.body;
+    const docRef = await addDoc(collection(db, "users-tabs"), {
+      user: userName,
+      band: bandName,
+      song: songnName,
+      guitar: guitar || "empty",
+      bass: bass || "empty",
+      guitarTab: guitarTab || "empty",
+      bassTab: bassTab || "empty",
+    });
 
-  //En desarrollo
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.log("Error adding document: ", e);
+  }
 });
 
 module.exports = router;
