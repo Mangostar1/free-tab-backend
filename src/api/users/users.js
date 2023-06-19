7d81bf5b759aff4b608eda415d1a2a4526b32565
Omar <93488662+Mangostar1@users.noreply.github.com>
2023-06-18 21:18:12 -0400

    se elimina funcion que verifica autenticacion del usuario ya que en el index se autentica si el usuario esta o no logeado para poder acceder a la ruta usuario

--- src/api/users/users.js
+++ src/api/users/users.js
@@ -3,32 +3,23 @@ const router = express.Router();
 const dotenv = require("dotenv");
 dotenv.config();
 
-const authMiddleware = require("../../middleware/authMiddleware.js");
-
 //firebase
 const app = require("../../config/firebaseConfig.js");
 const { getAuth, onAuthStateChanged } = require("firebase/auth");
 
 const auth = getAuth(app);
 
-// Middleware de autenticación
-/* function isAuthenticated(req, res, next) {
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      req.user = user; // Almacenar el objeto de usuario en la solicitud
-      next();
-    } else {
-      res.status(401).json({ message: "Usuario no autenticado" });
-    }
-  });
-} */
-
 // Ruta protegida para obtener los datos del usuario
-router.get("/user-data", authMiddleware, (req, res) => {
+router.get("/user-data", (req, res) => {
   try {
-    const user = req.user;
-    const { email, displayName, photoURL } = user; // Obtener el uid y el email del usuario
-    res.json({ email, displayName, photoURL });
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { email, displayName, photoURL } = user; // Obtener el uid y el email del usuario
+        res.json({ email, displayName, photoURL });
+      } else {
+        res.status(401).json({ message: "Usuario no autenticado" });
+      }
+    });
   } catch (error) {
     res.status(400).json({ message: error });
   }
